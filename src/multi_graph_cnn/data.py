568f29aa6027ee4953a0a33b830138929ef91770
hugopavy <hugo.pavy@etu.minesparis.psl.eu>
2025-12-03 17:48:20 +0000

    change in the data

--- src/multi_graph_cnn/data.py
+++ src/multi_graph_cnn/data.py
@@ -1,6 +1,7 @@
 """Import the data to feed the model"""
 
 from scipy.io import loadmat
+import scipy.sparse as sp
 import numpy as np
 import h5py
 
@@ -24,7 +25,28 @@ def read_data(name_data):
     file_path = "data/" + name_data + "/" + data_names[name_data]
     with h5py.File(file_path, "r") as f:
         data = extract_nested_dict(f, type_element=np.array)
+    if name_data == "synthetic_netflix":
+        M = np.asarray(data['M']).astype(np.float32).T
+        O_training = np.asarray(data['Otraining']).astype(np.float32).T
+        O_test = np.asarray(data['Otest']).astype(np.float32).T
+        W_row = find_sparse_matrix(data["Wrow"])
+        W_col = find_sparse_matrix(data["Wcol"])
+        data = {
+            "M": M,  # 150x200
+            "O_training": O_training,  # 150x200
+            "O_test": O_test,  # 150x200
+            "W_row": W_row,  # 150x150
+            "W_col": W_col  # 200x200
+        }
     return data
+
+
+def find_sparse_matrix(dict_values):
+    """Find the sparse matrix from the representation we have"""
+    data = np.asarray(dict_values['data'])
+    ir = np.asarray(dict_values['ir'])
+    jc = np.asarray(dict_values['jc'])
+    return sp.csc_matrix((data, ir, jc)).astype(np.float32)
 
 
 def split_data(data, config):
@@ -33,5 +55,5 @@ def split_data(data, config):
 
 
 if __name__ == "__main__":
-    data = read_data("yahoo_music")
+    data = read_data("synthetic_netflix")
     print("oh")
