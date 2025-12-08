212d729810237bae75ad9d09178494f63a530e7a
t-mariani <tom.mariani68@gmail.com>
2025-12-08 22:24:13 +0100

    IMP: add docstring

--- src/multi_graph_cnn/visualization.py
+++ src/multi_graph_cnn/visualization.py
@@ -4,6 +4,13 @@ import pandas as pd
 import seaborn as sns 
 
 def plot_coef_influence(coefs, title, ax=None, show=True):
+    """Plot the influence of convolution coefficients on the graph convolution.
+    Args:
+        coefs (np.ndarray): Coefficients of the convolution.
+        title (str): Title of the plot.
+        ax (matplotlib.axes.Axes, optional): Axes to plot on. Defaults to None.
+        show (bool, optional): Whether to show the plot. Defaults to True.
+    """
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=(8, 8))
     
@@ -17,6 +24,13 @@ def plot_coef_influence(coefs, title, ax=None, show=True):
 
 
 def plot_matrix_completion(matrix, title, ax=None, show=True):
+    """Plot a the matrix completion as a heatmap
+    Args:
+        matrix (np.ndarray): The matrix to plot.
+        title (str): Title of the plot.
+        ax (matplotlib.axes.Axes, optional): Axes to plot on. Defaults to None.
+        show (bool, optional): Whether to show the plot. Defaults to True.
+    """
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=(8, 8))
 
@@ -29,6 +43,14 @@ def plot_matrix_completion(matrix, title, ax=None, show=True):
     if show: plt.show()
 
 def plot_matrix_completion_duo(matrix1, matrix2, title1, title2, ax=None, show=True):
+    """Plot two matrices as a side-by-side heatmap.
+    Args:
+        matrix1 (np.ndarray): The first matrix to plot.
+        matrix2 (np.ndarray): The second matrix to plot.
+        title1 (str): Title of the first plot.
+        title2 (str): Title of the second plot.
+        ax (matplotlib.axes.Axes, optional): Axes to plot on. Defaults to None.
+        show (bool, optional): Whether to show the plot. Defaults to True."""
     if ax is None:
         _, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 8))
     else:
@@ -50,6 +72,14 @@ def plot_matrix_completion_duo(matrix1, matrix2, title1, title2, ax=None, show=T
     if show: plt.show()
 
 def plot_error_distribution(df_error:pd.DataFrame, groupby:bool=False, ax=None, show=True):
+    """Plot the distribution of errors in the matrix completion.
+    Args:
+        df_error (pd.DataFrame): DataFrame with 2 columns "error" and "split"
+        Note that split would probably be between "train", "target", "test" or "true"
+        groupby (bool, optional): Whether to group by split. Defaults to False.
+        ax (matplotlib.axes.Axes, optional): Axes to plot on. Defaults to None.
+        show (bool, optional): Whether to show the plot. Defaults to True.
+    """
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=(8, 9))
 
