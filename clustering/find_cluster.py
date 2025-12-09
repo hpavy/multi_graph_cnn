1bac97cdc80cbbf7ee38081bfd635447c0c45a23
Valentin EXBRAYAT <valentin.exbrayat@etu.minesparis.psl.eu>
2025-12-09 15:03:29 +0100

    update W clustering

--- clustering/find_cluster.py
+++ clustering/find_cluster.py
@@ -1,6 +1,7 @@
 import numpy as np
 import networkx as nx
 from scipy.linalg import eigh
+from sklearn.cluster import KMeans, SpectralClustering
 
 def find_cluster_from_graph(A):
     """Find the best cluster possible from a graph"""
@@ -17,3 +18,31 @@ def find_cluster_from_graph(A):
     # Assign clusters based on the sign of the Fiedler vector
     clusters = np.sign(fiedler_vector)
     return clusters 
+
+def cluster_linear_embedding(X):
+    # KMeans de scikit-learn fonctionne sur des numpy arrays
+    X = X.detach().cpu().numpy()
+    kmeans = KMeans(n_clusters=2, random_state=42)
+    labels = kmeans.fit_predict(X)
+    return(labels)
+
+def cluster_Spectral_embedding(X):
+    # KMeans de scikit-learn fonctionne sur des numpy arrays
+    X = X.detach().cpu().numpy()
+    clustering = SpectralClustering(n_clusters=2, affinity='nearest_neighbors', random_state=42)
+    labels = clustering.fit_predict(X)
+    return(labels)
+
+def clustering_accuracy(y_true, y_pred):
+    """
+    y_true : labels initiaux (taille n)
+    y_pred : labels prédits (taille n)
+    """
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    acc1 = (y_true == y_pred).mean()
+
+    acc2 = (y_true == (1 - y_pred)).mean()
+
+    return max(acc1, acc2)
--- clustering/main.py
+++ clustering/main.py
@@ -3,109 +3,132 @@
 from datetime import datetime
 from pathlib import Path
 
+import argparse
 import random
 import torch
 import numpy as np
+import json
 from torchinfo import summary
 
 from multi_graph_cnn.utils import get_logger, load_config
 from multi_graph_cnn.data import read_data, split_data, compute_the_laplacians
-from multi_graph_cnn.model import MGCNN
-from multi_graph_cnn.training import train_loop
-from multi_graph_cnn.loss import rmse, DirichletReguLoss
-from multi_graph_cnn.utils import sparse_mx_to_torch
+from multi_graph_cnn.model import sRGCNN
+from multi_graph_cnn.training import  train_loop_sRGCNN
+from multi_graph_cnn.loss import rmse, DirichletReguLossSRGCNN
+from multi_graph_cnn.utils import sparse_mx_to_torch, get_svd_initialization
 from multi_graph_cnn.test import run_tests
 
 from multi_graph_cnn.utils import get_tensorboard_writer
 
-from clustering import create_cluster, create_stochastic_graph, generate_data
+from clustering import create_cluster, create_stochastic_graph, generate_data,clustering_accuracy, find_cluster_from_graph, cluster_Spectral_embedding
 from utils import compute_the_laplacians, split_ranking
 
 
 if __name__ == "__main__":
-    values_proba_within = [20, 40]
-    for proba_within in values_proba_withing:
-        config = load_config("clustering/clustering_config.yaml")
-        log = get_logger("main", config.log_level)
-        log.debug(config)
+    config = load_config("clustering/clustering_config.yaml")
+    log = get_logger("main", config.log_level)
+    log.debug(config)
 
-        seed = config.seed
-        random.seed(seed)
-        np.random.seed(seed)
-        torch.manual_seed(seed)
+    seed = config.seed
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
 
-        # creating the clusters and the graphs
-        user_tastes, movie_kinds = create_cluster(config)
-        graph_users = create_stochastic_graph(
-            user_tastes, config.proba_within_users, config.nb_neighbour_users
-            )
-        graph_movies = create_stochastic_graph(
-            movie_kinds, config.proba_within_movies, config.nb_neighbour_movies
-            )
+    # creating the clusters and the graphs
+    user_tastes, movie_kinds = create_cluster(config)
+    graph_users = create_stochastic_graph(
+        user_tastes, config.proba_within_users, config.nb_neighbour_users
+        )
+    graph_movies = create_stochastic_graph(
+        movie_kinds, config.proba_within_movies, config.nb_neighbour_movies
+        )
+    accuracy ={}
 
-        # creating the ranking
-        ranking = generate_data(user_tastes, movie_kinds, config)
+    cluster_G_m = find_cluster_from_graph(graph_movies)
+    cluster_G_u = find_cluster_from_graph(graph_users)
+    accuracy["accuracy_u_init"] = clustering_accuracy(cluster_G_u, user_tastes)
+    accuracy["accuracy_m_init"] = clustering_accuracy(cluster_G_m, movie_kinds)
 
-        O_training, O_target, O_test = split_ranking(ranking, config)
-        L_row = compute_the_laplacians(graph_users)
-        L_col = compute_the_laplacians(graph_movies)
+    # creating the ranking
+    ranking = generate_data(user_tastes, movie_kinds, config)
 
-        now = datetime.now().strftime("%Y%m%d-%H%M%S")
-        dir_path = Path("clustering/saved_models/" + now)
-        config.output_dir = str(dir_path)
-        config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    O_training, O_target, O_test = split_ranking(ranking, config)
+    L_row = compute_the_laplacians(graph_users)
+    L_col = compute_the_laplacians(graph_movies)
 
-        L_row = torch.tensor(L_row, device=config.device, dtype=torch.float32)
-        L_col = torch.tensor(L_col, device=config.device, dtype=torch.float32)
+    model_id = config.proba_within_users + config.proba_within_movies 
+    dir_path = Path("clustering/saved_models/" + model_id)
+    config.output_dir = str(dir_path)
+    config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    parser = argparse.ArgumentParser(description="Entraînement Recommender System (MGCNN vs sRGCNN)")
 
-        L_row_rescaled = L_row - torch.eye(L_row.shape[0], device=config.device)
-        L_col_rescaled = L_col - torch.eye(L_col.shape[0], device=config.device)
+    model_type ="sRGCNN"
 
-        model = MGCNN(L_row_rescaled, L_col_rescaled, config)
-        model = model.to(config.device)
-        summary(
-            model,
-            depth=5,
-            input_size=(ranking.shape[0], ranking.shape[1]),
-            )
+    L_row = sparse_mx_to_torch(L_row).to(config.device)
+    L_col = sparse_mx_to_torch(L_col).to(config.device)
 
-        optimizer = torch.optim.AdamW(
-            model.parameters(),
-            lr=float(config.learning_rate),
-            )
+    L_row_rescaled = L_row - torch.eye(L_row.shape[0], device=config.device)
+    L_col_rescaled = L_col - torch.eye(L_col.shape[0], device=config.device)
 
-        loss = DirichletReguLoss(L_row, L_col, config)
-        loss_rmse = rmse
+    W,H  = get_svd_initialization(ranking,config.rank, config.device)
 
-        data = torch.tensor(ranking, dtype=torch.float32).to(config.device)
-        O_training = torch.tensor(O_training, dtype=torch.float32).to(config.device)
-        O_test = torch.tensor(O_test, dtype=torch.float32).to(config.device)
-        O_target = torch.tensor(O_target, dtype=torch.float32).to(config.device)
+    model = sRGCNN(L_row_rescaled, L_col_rescaled, config)
+    model = model.to(config.device)
+    summary(
+        model,
+        depth=5,
+        input_data = [W,H],
+        )
 
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=float(config.learning_rate),
+        # weight_decay=config.weight_decay,
+        )
+
+    loss = DirichletReguLossSRGCNN(L_row, L_col, config)
+    loss_rmse = rmse
+
+    data = torch.tensor(ranking).to(config.device)
+    O_training = torch.tensor(O_training).to(config.device)
+    O_test = torch.tensor(O_test).to(config.device)
+    O_target = torch.tensor(O_target).to(config.device)
+
+    if not dir_path.exist():
         # Initialize TensorBoard Writer
         writer = get_tensorboard_writer(config)
         log.info(f" TensorBoard logging enabled in: {config.output_dir}/{config.tensorboard_dir}")
-
+        
         log.info("Starting training...")
         try:
-            train_loop(model, data, O_training, O_target, O_test, optimizer, loss, loss_rmse, config, writer)
+            train_loop_sRGCNN(model, W,H,data, O_training, O_target, O_test, optimizer, loss, loss_rmse, config, writer)
         except KeyboardInterrupt:
             log.warning("Training interrupted by user.")
 
         # Close the writer
         writer.close()
 
-        # [OPTIONAL] Load the best model before running final tests
-        best_model_path = f"{config.output_dir}/best_model.pth"
-        try:
-            model.load_state_dict(torch.load(best_model_path))
-            log.info("🏆 Loaded best model for final testing.")
-        except Exception as e:
-            log.warning("Could not load best model (maybe none was saved?), using last state.")
+    # [OPTIONAL] Load the best model before running final tests
+    best_model_path = f"{config.output_dir}/best_model.pth"
+    try:
+        checkpoint = torch.load(best_model_path)
+        model.load_state_dict(checkpoint)
+        W = checkpoint['W']
+        H = checkpoint['H']
+        cluster_W = cluster_Spectral_embedding(W)
+        cluster_H = cluster_Spectral_embedding(H)
+        accuracy["accuracy_W"] = clustering_accuracy(cluster_W, user_tastes)
+        accuracy["accuracy_H"] = clustering_accuracy(cluster_H, movie_kinds)
+        with open(f"{config.output_dir}/accuracy.json", "w") as f:
+            json.dump(accuracy, f, indent=4) 
+        log.info("🏆 Loaded best model for final testing.")
+        log.info(accuracy)
+    except Exception as e:
+        log.warning("Could not load best model (maybe none was saved?), using last state.")
 
-        run_tests(model, data, O_training, O_target, O_test, loss, loss_rmse, config)
-
-        log.info("✅ Pipeline completed successfully")
+    run_tests(model, data, O_training, O_target, O_test, loss, loss_rmse, config)
+    
+    log.info("✅ Pipeline completed successfully")
