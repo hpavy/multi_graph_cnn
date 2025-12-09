8b13adf073d719a24e13d86536f8649a4c500fa1
valentinexbrayat <valentin.exbrayat@etu.minesparis.psl.eu>
2025-12-09 14:46:04 +0000

    test 1 : W embedding accuracy bad

--- clustering/main.py
+++ clustering/main.py
@@ -20,7 +20,8 @@ from multi_graph_cnn.test import run_tests
 
 from multi_graph_cnn.utils import get_tensorboard_writer
 
-from clustering import create_cluster, create_stochastic_graph, generate_data,clustering_accuracy, find_cluster_from_graph, cluster_Spectral_embedding
+from clustering import create_cluster, create_exact_neighbours_graph, generate_data
+from find_cluster import clustering_accuracy, find_cluster_from_graph, cluster_Spectral_embedding
 from utils import compute_the_laplacians, split_ranking
 
 
@@ -39,12 +40,13 @@ if __name__ == "__main__":
 
     # creating the clusters and the graphs
     user_tastes, movie_kinds = create_cluster(config)
-    graph_users = create_stochastic_graph(
+    graph_users = create_exact_neighbours_graph(
         user_tastes, config.proba_within_users, config.nb_neighbour_users
         )
-    graph_movies = create_stochastic_graph(
+    graph_movies = create_exact_neighbours_graph(
         movie_kinds, config.proba_within_movies, config.nb_neighbour_movies
         )
+
     accuracy ={}
 
     cluster_G_m = find_cluster_from_graph(graph_movies)
@@ -59,7 +61,7 @@ if __name__ == "__main__":
     L_row = compute_the_laplacians(graph_users)
     L_col = compute_the_laplacians(graph_movies)
 
-    model_id = config.proba_within_users + config.proba_within_movies 
+    model_id = str(config.proba_within_users) + '_' + str(config.proba_within_movies)
     dir_path = Path("clustering/saved_models/" + model_id)
     config.output_dir = str(dir_path)
     config.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -67,8 +69,8 @@ if __name__ == "__main__":
 
     model_type ="sRGCNN"
 
-    L_row = sparse_mx_to_torch(L_row).to(config.device)
-    L_col = sparse_mx_to_torch(L_col).to(config.device)
+    L_row = torch.tensor(L_row, dtype=torch.float32).to(config.device)
+    L_col = torch.tensor(L_col, dtype=torch.float32).to(config.device)
 
     L_row_rescaled = L_row - torch.eye(L_row.shape[0], device=config.device)
     L_col_rescaled = L_col - torch.eye(L_col.shape[0], device=config.device)
@@ -97,7 +99,7 @@ if __name__ == "__main__":
     O_test = torch.tensor(O_test).to(config.device)
     O_target = torch.tensor(O_target).to(config.device)
 
-    if not dir_path.exist():
+    if not dir_path.exists():
         # Initialize TensorBoard Writer
         writer = get_tensorboard_writer(config)
         log.info(f" TensorBoard logging enabled in: {config.output_dir}/{config.tensorboard_dir}")
@@ -115,7 +117,6 @@ if __name__ == "__main__":
     best_model_path = f"{config.output_dir}/best_model.pth"
     try:
         checkpoint = torch.load(best_model_path)
-        model.load_state_dict(checkpoint)
         W = checkpoint['W']
         H = checkpoint['H']
         cluster_W = cluster_Spectral_embedding(W)
@@ -124,11 +125,15 @@ if __name__ == "__main__":
         accuracy["accuracy_H"] = clustering_accuracy(cluster_H, movie_kinds)
         with open(f"{config.output_dir}/accuracy.json", "w") as f:
             json.dump(accuracy, f, indent=4) 
-        log.info("🏆 Loaded best model for final testing.")
+        
         log.info(accuracy)
+
+        model.load_state_dict(torch.load(best_model_path))
+        log.info("🏆 Loaded best model for final testing.")
     except Exception as e:
+        print(e)
         log.warning("Could not load best model (maybe none was saved?), using last state.")
 
-    run_tests(model, data, O_training, O_target, O_test, loss, loss_rmse, config)
+   # run_tests(model, data, O_training, O_target, O_test, loss, loss_rmse, config)
     
     log.info("✅ Pipeline completed successfully")
